d006e121e315a1586b68a745d502b0ab76656d68
Merrily Tan <merrilytan@gmail.com>
2020-10-24 18:14:30 -0400

    Updated navbar to close other dropdown when another dropdown is opened

--- src/components/nav/mainNav.tsx
+++ src/components/nav/mainNav.tsx
@@ -1,11 +1,4 @@
-import {
-  Navbar,
-  NavbarBrand,
-  NavLink,
-  NavbarToggler,
-  Collapse,
-  Nav,
-} from "reactstrap";
+import { Navbar, NavbarBrand, NavLink, Collapse, Nav } from "reactstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars, faPlusCircle } from "@fortawesome/free-solid-svg-icons";
 import { useState } from "react";
@@ -15,11 +8,15 @@ import AddToBubble from "src/components/addToBubble/addToBubble";
 import styles from "./mainNav.module.scss";
 
 const MainNav: React.FC = () => {
-  const [isNavOpen, setIsNavOpen] = useState(false);
-  const [isAddOpen, setIsAddOpen] = useState(false);
+  const [navStatus, setNavStatus] = useState({
+    isNavOpen: false,
+    isAddOpen: false,
+  });
 
-  const toggleNav = () => setIsNavOpen(!isNavOpen);
-  const toggleAdd = () => setIsAddOpen(!isAddOpen);
+  const toggleNav = () =>
+    setNavStatus({ isNavOpen: !navStatus.isNavOpen, isAddOpen: false });
+  const toggleAdd = () =>
+    setNavStatus({ isAddOpen: !navStatus.isAddOpen, isNavOpen: false });
 
   return (
     <Navbar expand="md" light fixed="top" className={styles.navBar}>
@@ -34,13 +31,13 @@ const MainNav: React.FC = () => {
         <FontAwesomeIcon icon={faPlusCircle} />
       </Nav>
 
-      <Collapse navbar isOpen={isNavOpen}>
+      <Collapse navbar isOpen={navStatus.isNavOpen}>
         <NavLink onClick={authService.signOut} role="button">
           Logout
         </NavLink>
       </Collapse>
 
-      <Collapse navbar isOpen={isAddOpen}>
+      <Collapse navbar isOpen={navStatus.isAddOpen}>
         <AddToBubble />
       </Collapse>
     </Navbar>
