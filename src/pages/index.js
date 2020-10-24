e9651a60dd4e6e2c3931451ca36f922cff3761a9
mrkprinc <36653433+mrkprinc@users.noreply.github.com>
2020-10-23 20:10:00 -0400

    setup src/, sass, ts (#2)

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js w/ Firebase Client-Side</title>
+        <title>Who's in my bubble?</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
