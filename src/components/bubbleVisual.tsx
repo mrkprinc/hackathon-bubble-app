a073e2cdb68d9cd60927c7f52d20f706896e1f02
Andrea Cabral <acabral@qmo.io>
2020-10-24 13:59:52 -0400

    Adding nodes and links with real data

--- src/components/bubbleVisual.tsx
+++ src/components/bubbleVisual.tsx
@@ -7,7 +7,7 @@ type BubbleVisualProps = {};
 
 const BubbleVisual: React.FC<BubbleVisualProps> = ({ children }) => (
   <div className={styles.container}>
-    <img src="/example.png" alt="Example" className={styles.visualization} />
+    {children}
   </div>
 );
 
--- src/pages/d3/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import data from '../../data/data.json'
-import { ForceGraph } from '../../components/force-graph'
-
-const NetworkGraph: React.FC = () => <ForceGraph linksData={data.links as any} nodesData={data.nodes as any} />
-
-export default NetworkGraph
