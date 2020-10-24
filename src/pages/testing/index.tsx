f4937b602eb7c3e5904fef8a4cc39dab8fba6b16
Andrea Cabral <acabral@qmo.io>
2020-10-23 22:44:58 -0400

    Add D3 example

---
+++ src/pages/testing/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import data from '../../data/data.json'
+import { ForceGraph } from '../../components/forceGraph'
+
+function App() {
+  const nodeHoverTooltip = React.useCallback((node) => {
+    return `<div>${node.name}</div>`
+  }, [])
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        Force Graph Example
+      </header>
+      <section className="Main">
+        <ForceGraph linksData={data.links} nodesData={data.nodes} nodeHoverTooltip={nodeHoverTooltip} />
+      </section>
+    </div>
+  )
+}
+
+export default App
