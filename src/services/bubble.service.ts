710a6af92ee2ecf1e45ec623398110a7964c05f0
Mark Prince <mprince@qmo.io>
2020-10-24 10:20:02 -0400

    Add to bubble by email

--- src/services/bubble.service.ts
+++ src/services/bubble.service.ts
@@ -3,7 +3,10 @@ import Collections from 'src/firebase/collections.enum'
 import { User } from 'src/context/userContext'
 
 async function getUserBubble(user: User) {
-  if (!user?.connectedUsers?.length) return [];
+  if (!user?.connectedUsers?.length) {
+    console.error("BubbleService: No connected users.")
+    return [];
+  }
 
   const connectedUsers = await firebase.firestore()
     .collection(Collections.USERS)
@@ -15,6 +18,33 @@ async function getUserBubble(user: User) {
   return connectedUsers;
 }
 
+async function addToBubbleByEmail(currentUser: User, email: string) {
+  const db = firebase.firestore()
+  const userToAddSnapshot = await db.collection(Collections.USERS)
+    .where('email', '==', email)
+    .get()
+    .then((snapshot) => snapshot.docs[0]);
+
+  if (!userToAddSnapshot) {
+    console.error('BubbleService: No user with that email.');
+    return false;
+  }
+
+  const currentUserRef = db.collection(Collections.USERS).doc(currentUser.id);
+  const batch = db.batch();
+  batch.update(currentUserRef, {
+    connectedUsers: firebase.firestore.FieldValue.arrayUnion(userToAddSnapshot.id),
+  });
+  batch.update(userToAddSnapshot.ref, {
+    connectedUsers: firebase.firestore.FieldValue.arrayUnion(currentUser.id),
+  });
+  await batch.commit();
+  console.log('Added to bubble:', userToAddSnapshot.id)
+
+  return true;
+}
+
 export default {
   getUserBubble,
+  addToBubbleByEmail,
 }
