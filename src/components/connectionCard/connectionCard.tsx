d71f385b06f42e2cc325ec9b94bf864e386d26f8
mrkprinc <36653433+mrkprinc@users.noreply.github.com>
2020-10-25 15:35:48 -0400

    Profile style updates (#38)

--- src/components/connectionCard/connectionCard.tsx
+++ src/components/connectionCard/connectionCard.tsx
@@ -16,25 +16,25 @@ interface ConnectionProps {
 const ConnectionCard: React.FC<ConnectionProps> = ({ connection, mutualConnections, totalConnections }) => {
   const { user } = useUser();
   return (
-    <Container fluid className={'mt-1 mb-1'}>
+    <Container fluid className="py-4 mt-4 border">
       <Row>
         <Col xs="auto" sm="auto">
-          <img src={connection.photoURL || '/avatar_placeholder.jpg'} alt="avatar" className="rounded-circle" width="100" height="100" />
+          <div style={{ width: '75px', height: '75px' }}>
+            <img src={connection.photoURL || '/avatar_placeholder.jpg'} alt="avatar" className="rounded-circle" style={{ width: '100%', height: '100%', objectFit: 'contain' }} />
+          </div>
         </Col>
-        <Col xs="auto" sm="auto">
-          <CardTitle>{connection.displayName}</CardTitle>
-           <CardSubtitle>
+        <Col xs="auto" sm="auto" className="flex-grow-1">
+          <CardTitle className="font-weight-bolder">{connection.displayName}</CardTitle>
+           <CardSubtitle className="mb-1">
             Mutual connections <Badge color="info">{mutualConnections}</Badge>
           </CardSubtitle>
-          <CardSubtitle>
+          <CardSubtitle className="mb-1">
             Total connections <Badge color="info">{totalConnections}</Badge>
           </CardSubtitle>
           {connection.externalOrg && <Badge color="warning">Work/School</Badge>}
         </Col>
-        <Col xs={12} sm="auto">
-          <div style={{ display: 'flex', flex:1, flexDirection: "row-reverse"}}>
-            <Button color="danger" onClick={() => bubbleService.removeFromBubble(user, connection)}>Delete</Button>
-          </div>
+        <Col xs={12} sm="auto" className="align-self-center">
+          <Button color="danger" onClick={() => bubbleService.removeFromBubble(user, connection)}>Delete</Button>
         </Col>
       </Row>
     </Container>
