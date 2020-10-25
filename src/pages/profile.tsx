56fc9331454308eb01214d0dea7577683332beaa
Andrea Cabral <acabral@qmo.io>
2020-10-25 16:58:35 -0400

    fix total connections

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -114,7 +114,7 @@ export default function Profile() {
                         user.connectedUsers,
                         userData.connectedUsers
                       )}
-                      totalConnections={userData.connectedUsers.length}
+                      totalConnections={userData.connectedUsers.length + (userData.extraBubbleMembers || 0)}
                     />
                   ) : null;
                 })}
